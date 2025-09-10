6104d3ed1211e07f3f61c12c35c6fcc243be5cc6
Ted Driggs <ted.driggs@outlook.com>
2025-09-10 10:06:15 -0700

    Add function for making new NSID consts

--- openc2/src/data/nsid.rs
+++ openc2/src/data/nsid.rs
@@ -16,14 +16,31 @@ impl Nsid {
     pub const SLPF: Self = Nsid(Cow::Borrowed("slpf"));
     pub const SFPF: Self = Nsid(Cow::Borrowed("sfpf"));
     pub const ER: Self = Nsid(Cow::Borrowed("er"));
+
+    /// Returns a new `Nsid` for a given string.
+    ///
+    /// # Panics
+    /// This function will panic if the provided string is not a valid NSID.
+    pub const fn new_const(value: &'static str) -> Self {
+        if Nsid::check(value).is_err() {
+            panic!("invalid NSID");
+        }
+
+        Self(Cow::Borrowed(value))
+    }
+
+    const fn check(s: &str) -> Result<(), &'static str> {
+        if s.len() > 16 {
+            return Err("NSID must be 16 characters or fewer");
+        }
+        Ok(())
+    }
 }
 
 impl TryFrom<String> for Nsid {
     type Error = ValidationError;
     fn try_from(value: String) -> Result<Self, Self::Error> {
-        if value.len() > 16 {
-            return Err(ValidationError::new("NSID must be 16 characters or fewer"));
-        }
+        Nsid::check(&value).map_err(ValidationError::new)?;
         Ok(Self(Cow::Owned(value)))
     }
 }
@@ -31,9 +48,7 @@ impl TryFrom<String> for Nsid {
 impl TryFrom<&'static str> for Nsid {
     type Error = ValidationError;
     fn try_from(value: &'static str) -> Result<Self, Self::Error> {
-        if value.len() > 16 {
-            return Err(ValidationError::new("NSID must be 16 characters or fewer"));
-        }
+        Nsid::check(&value).map_err(ValidationError::new)?;
         Ok(Self(Cow::Borrowed(value)))
     }
 }
@@ -41,9 +56,7 @@ impl TryFrom<&'static str> for Nsid {
 impl FromStr for Nsid {
     type Err = ValidationError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.len() > 16 {
-            return Err(ValidationError::new("NSID must be 16 characters or fewer"));
-        }
+        Nsid::check(&s).map_err(ValidationError::new)?;
 
         // For known profiles, reuse the const to avoid heap allocations
         match s {
