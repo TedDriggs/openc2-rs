3796df8608a60fd88aabcd1b20faeefa90e8f6c0
Ted Driggs <ted.driggs@outlook.com>
2025-09-19 14:26:54 -0700

    Add blanket Consume impls

--- openc2_consumer/src/lib.rs
+++ openc2_consumer/src/lib.rs
@@ -14,3 +14,44 @@ pub trait Consume {
     /// Handle an incoming OpenC2 command and produce a response.
     async fn consume(&self, msg: Message<Headers, Command>) -> Result<Response, Error>;
 }
+
+#[async_trait]
+impl<T: Consume + Sync + Send> Consume for Box<T> {
+    async fn consume(&self, msg: Message<Headers, Command>) -> Result<Response, Error> {
+        (**self).consume(msg).await
+    }
+}
+
+#[async_trait]
+impl<T: Consume + Sync + Send> Consume for std::sync::Arc<T> {
+    async fn consume(&self, msg: Message<Headers, Command>) -> Result<Response, Error> {
+        (**self).consume(msg).await
+    }
+}
+
+#[async_trait]
+impl<'a, T: Consume + Sync + Send + Clone> Consume for std::borrow::Cow<'a, T> {
+    async fn consume(&self, msg: Message<Headers, Command>) -> Result<Response, Error> {
+        (**self).consume(msg).await
+    }
+}
+
+#[async_trait]
+impl<T: Consume + Sync + Send> Consume for Option<T> {
+    async fn consume(&self, msg: Message<Headers, Command>) -> Result<Response, Error> {
+        match self {
+            Some(consumer) => consumer.consume(msg).await,
+            None => Err(Error::custom("no consumer available")),
+        }
+    }
+}
+
+#[async_trait]
+impl<T: Consume + Sync + Send> Consume for Result<T, Error> {
+    async fn consume(&self, msg: Message<Headers, Command>) -> Result<Response, Error> {
+        match self {
+            Ok(consumer) => consumer.consume(msg).await,
+            Err(e) => Err(e.clone()),
+        }
+    }
+}
